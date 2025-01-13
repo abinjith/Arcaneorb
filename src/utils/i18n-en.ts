bb5893b48da69f272101058b5e265abb7653e92b
abinjith <abinjith781@gmail.com>
2025-01-14 03:55:37 +0530

    verse1

---
+++ src/utils/i18n-en.ts
@@ -0,0 +1,25 @@
+/**
+ * A dictionary of strings used in-game
+ */
+export const i18nDict = {
+    pixiLogoHeader: 'Made with',
+    gameTitle: 'ArcaneOrb',
+    gameSubtitle: 'hexshooter',
+    titlePlay: 'SHOOT BUBBLES!',
+    forkGithub: 'Fork me\non Github',
+    helperMobile: 'Hold to aim\nRelease to shoot!',
+    helperDesktop: 'Move mouse to aim\nLeft click to shoot!',
+    paused: 'GAME PAUSED',
+    score: 'CURRENT SCORE',
+    sound: 'Sound',
+    resume: 'Resume',
+    quit: 'Quit',
+    resultsPlay: 'Attack AGAIN',
+    resultsTitle: 'YOUR SCORE!',
+    resultsBreakdownTitle: 'BREAKDOWN',
+    resultsBreakdownPopped: 'Bubbles popped',
+    resultsBreakdownPowerups: 'Powerups used',
+    resultsBreakdownCombo: 'Best combo',
+    resultsScoreTitle: 'FINAL SCORE',
+    resultsHighscoreTitle: 'HIGHSCORE',
+};
